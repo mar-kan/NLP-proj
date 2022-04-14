13690bd055ff233c37ab1811604e3840fa3abf1e
mar-kan <marianna@kanellakis.eu>
2022-04-14 03:41:03 +0300

    loop

--- Preprocess.py
+++ Preprocess.py
@@ -3,58 +3,54 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from collections import Counter
 from nltk.corpus import wordnet
-from pandas.core.dtypes.common import is_string_dtype
 from textblob import TextBlob
 import nltk
 import re
 
 
 def preprocess(dataset):
-    def preprocess(dataset):
-        for i in dataset.index:
-            # remove uppercase letters
-            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].lower() if type(dataset.loc[i, 'tweet']) == str else \
-                dataset.loc[i, 'tweet']
+    for i in dataset.index:
+        # remove uppercase letters
+        dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].lower() if type(dataset.loc[i, 'tweet']) == str else \
+            dataset.loc[i, 'tweet']
 
-            # convert emojis and emoticons to text
-            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(emojisToWords)
-            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(emoticonsToWords)
+        # convert emojis and emoticons to text
+        dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(emojisToWords)
+        dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(emoticonsToWords)
 
-            # remove URLs
-            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace(r'^https?:\/\/.*[\r\n]*', '')
+        # remove URLs
+        dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace(r'^https?:\/\/.*[\r\n]*', '')
 
-            # remove numbers
-            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace(r'\d+', '')
+        # remove numbers
+        dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace(r'\d+', '')
 
-            # remove \n, \u0111
-            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace('\n', '')
-            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace('\u0111', '')
+        # remove \n, \u0111
+        dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace('\n', '')
+        dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace('\u0111', '')
 
-            # remove punctuation
-            dataset['tweet'] = dataset['tweet'].str.replace('[^A-Za-z0-9-\s]+', '') if type(
-                dataset.loc[i, 'tweet']) == str else dataset.loc[i, 'tweet']
+        # remove punctuation
+        dataset['tweet'] = dataset['tweet'].str.replace('[^A-Za-z0-9-\s]+', '') if type(
+            dataset.loc[i, 'tweet']) == str else dataset.loc[i, 'tweet']
 
-            # remove stopwords
-            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(removeStopwords)
+        # remove stopwords
+        dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(removeStopwords)
 
-            # removes most common words
-            # cnt = findMostCommonWords(dataset)
-            # dataset.loc[i,'tweet'] = dataset.loc[i,'tweet'].apply(removeCommonWords)
+        # removes most common words
+        # cnt = findMostCommonWords(dataset)
+        # dataset.loc[i,'tweet'] = dataset.loc[i,'tweet'].apply(removeCommonWords)
 
-            # Rare word removal
+        # Rare word removal
 
-            # spelling correction
-            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(lambda x: str(TextBlob(x).correct()))
+        # spelling correction
+        dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(lambda x: str(TextBlob(x).correct()))
 
-            # remove whitespaces
-            # dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].str.strip()
+        # remove whitespaces
+        # dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].str.strip()
 
-            # tokenization
+        # tokenization
 
-            # lemmatization
-            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(lemmatize_words)
-
-        return dataset
+        # lemmatization
+        dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(lemmatize_words)
 
     return dataset
 
