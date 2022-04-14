ca2cc4c3aa7ac134815d648ed1efe9ec15583fa3
mar-kan <marianna@kanellakis.eu>
2022-04-14 03:35:22 +0300

    one big loop

--- Preprocess.py
+++ Preprocess.py
@@ -1,66 +1,73 @@
+from emot.emo_unicode import UNICODE_EMOJI, EMOTICONS_EMO
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from collections import Counter
 from nltk.corpus import wordnet
+from pandas.core.dtypes.common import is_string_dtype
 from textblob import TextBlob
-from emot import emo_unicode
 import nltk
 import re
 
 
 def preprocess(dataset):
-    # remove uppercase letters
-    dataset['tweet'] = dataset['tweet'].apply(lambda s: s.lower() if type(s) == str else s)
+    def preprocess(dataset):
+        for i in dataset.index:
+            # remove uppercase letters
+            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].lower() if type(dataset.loc[i, 'tweet']) == str else \
+                dataset.loc[i, 'tweet']
 
-    # convert emojis and emoticons to text
-    dataset['tweet'] = dataset['tweet'].apply(emojisToWords)
-    #dataset['tweet'] = dataset['tweet'].apply(emoticonsToWords)
+            # convert emojis and emoticons to text
+            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(emojisToWords)
+            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(emoticonsToWords)
 
-    # remove URLs
-    dataset['tweet'] = dataset['tweet'].replace(to_replace=r'^https?:\/\/.*[\r\n]*', value='', regex=True)
+            # remove URLs
+            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace(r'^https?:\/\/.*[\r\n]*', '')
 
-    # remove numbers
-    dataset['tweet'] = dataset['tweet'].replace(to_replace=r'\d+', value='', regex=True)
+            # remove numbers
+            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace(r'\d+', '')
 
-    # remove \n, \u0111
-    dataset['tweet'] = dataset['tweet'].replace(to_replace='\n', value='', regex=True)
-    dataset['tweet'] = dataset['tweet'].replace(to_replace='\u0111', value='', regex=True)
+            # remove \n, \u0111
+            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace('\n', '')
+            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].replace('\u0111', '')
 
-    # remove punctuation
-    dataset['tweet'] = dataset['tweet'].str.replace('[^\w\s]', '')  # replace(r'^https?:\/\/.*[\r?\n\t@]*', '')
+            # remove punctuation
+            dataset['tweet'] = dataset['tweet'].str.replace('[^A-Za-z0-9-\s]+', '') if type(
+                dataset.loc[i, 'tweet']) == str else dataset.loc[i, 'tweet']
 
-    # remove stopwords
-    dataset['tweet'] = dataset['tweet'].apply(removeStopwords)
+            # remove stopwords
+            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(removeStopwords)
 
-    # removes most common words
-    cnt = findMostCommonWords(dataset)
-    dataset['tweet'] = dataset["tweet"].apply(removeCommonWords)
+            # removes most common words
+            # cnt = findMostCommonWords(dataset)
+            # dataset.loc[i,'tweet'] = dataset.loc[i,'tweet'].apply(removeCommonWords)
 
-    # Rare word removal
+            # Rare word removal
 
-    # spelling correction
-    dataset['tweet'].apply(lambda x: str(TextBlob(x).correct()))
+            # spelling correction
+            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(lambda x: str(TextBlob(x).correct()))
 
-    # remove whitespaces
-    # dataset['tweet'].str.strip()
+            # remove whitespaces
+            # dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].str.strip()
 
-    # tokenization
+            # tokenization
 
-    # Lemmatization
-    dataset['tweet'] = dataset['tweet'].apply(lemmatize_words)
+            # lemmatization
+            dataset.loc[i, 'tweet'] = dataset.loc[i, 'tweet'].apply(lemmatize_words)
+
+        return dataset
 
     return dataset
 
 
 def emojisToWords(text):
-    for emot in emo_unicode.EMOJI_UNICODE:
-        text = text.replace(emot, "_".join(emo_unicode.EMOJI_UNICODE[emot].replace(",", "").replace(":", "").split()))
+    for emoji in UNICODE_EMOJI:
+        text = text.replace(emoji, "_".join(UNICODE_EMOJI[emoji].replace(",", "").replace(":", "").split()))
         return text
 
 
 def emoticonsToWords(text):
-    for emot in emo_unicode.EMOTICONS_EMO:
-        text = re.sub(u'(' + emot + ')', "_".join(emo_unicode.EMOTICONS_EMO[emot].replace(",", "").split()), text)
+    for emoticon in EMOTICONS_EMO:
+        text = re.sub(u'(' + emoticon + ')', "_".join(EMOTICONS_EMO[emoticon].replace(",", "").split()), text)
         return text
 
 
@@ -83,9 +90,11 @@ def removeCommonWords(text, toRemove, cnt):
     return " ".join([word for word in str(text).split() if word not
                      in cnt.most_common(toRemove)])
 
+
 def lemmatize_words(text):
     lemmatizer = WordNetLemmatizer()
     wordnet_map = {"N": wordnet.NOUN, "V": wordnet.VERB, "J": wordnet.ADJ, "R": wordnet.ADV}
 
     pos_tagged_text = nltk.pos_tag(text.split())
-    return " ".join([lemmatizer.lemmatize(word, wordnet_map.get(pos[0], wordnet.NOUN)) for word, pos in pos_tagged_text])
+    return " ".join(
+        [lemmatizer.lemmatize(word, wordnet_map.get(pos[0], wordnet.NOUN)) for word, pos in pos_tagged_text])
